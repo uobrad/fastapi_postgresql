00a84bf6433b642637f8f8091c03cfffee641974
dante <uros_obradovic@yahoo.com>
2022-09-07 15:09:58 +0200

    sredjena konekcija - kao i takodje alembic migracija

--- database/connection.py
+++ database/connection.py
@@ -2,9 +2,9 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from config import settings
+from database.config import settings
 
-DATABASE_URL = f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
+DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOSTNAME}:{settings.DATABASE_PORT}/{settings.POSTGRES_DB}"
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 import uuid
-from connection import Base
+from database.connection import Base
 from sqlalchemy import TIMESTAMP, Column, ForeignKey, String, Boolean, text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
@@ -37,3 +37,72 @@ class Post(Base):
     updated_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text("now()"))
     user = relationship('User')
+
+# from typing import Optional, List
+#
+# from sqlmodel import SQLModel, Field, Relationship
+
+from database.connection import Base
+
+
+# class SongBase(SQLModel):
+#     name: str
+#     artist: str
+#     year: Optional[int] = None
+
+
+# class Song(SongBase, table=True):
+#     id: int = Field(default=None, primary_key=True)
+
+
+# class SongCreate(SongBase):
+#     pass
+
+# class UserBase(SQLModel):
+#     name: str = Field(index=True)
+#     password: str
+#     age: Optional[int] = Field(default=None, index=True)
+#
+# class User(UserBase, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#
+#     posts: List["Post"] = Relationship(back_populates="user")
+#
+#
+# class UserCreate(UserBase):
+#     pass
+#
+#
+# class UserRead(UserBase):
+#     id: int
+#
+# class UserUpdate(SQLModel):
+#     name: Optional[str] = None
+#     secret_name: Optional[str] = None
+#     age: Optional[int] = None
+#
+# class PostBase(SQLModel):
+#     title: str = Field(index=True)
+#     content: str
+#     category: str = Field(index=True)
+#
+#     user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+#
+#
+# class Post(PostBase, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#
+#     user: Optional[User] = Relationship(back_populates="posts")
+#
+#
+# class PostCreate(PostBase):
+#     pass
+#
+#
+# class PostRead(PostBase):
+#     id: int
+#
+#
+# class PostUpdate(SQLModel):
+#     name: Optional[str] = None
+#     headquarters: Optional[str] = None
